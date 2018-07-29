cfeb79ee061cdaf56cf60c12e98f784921e8e00c
Nikhil Gupta <ngupta@ep.com>
2018-07-29 12:43:25 -0700

    added unreviewd api

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const RETRIEVE_PROJECTS_URL = "https://utopian.rocks/api/posts?status=pending";
+const RETRIEVE_PENDING_URL = "https://utopian.rocks/api/posts?status=pending";
+const RETRIEVE_UNREVIEWED_URL = "https://utopian.rocks/api/posts?status=unreviewed";
 const request = require("request");
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -20,7 +21,19 @@ app.use(routes.registerRoutes(router, app));
 */
 app.get('/api/pending', (req, res) => {
     request.get({
-        url: RETRIEVE_PROJECTS_URL,
+        url: RETRIEVE_PENDING_URL,
+        json: true
+    }, function (error, response, body) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.status(200).send(body);
+    });
+});
+app.get('/api/unreviewed', (req, res) => {
+    request.get({
+        url: RETRIEVE_UNREVIEWED_URL,
         json: true
     }, function (error, response, body) {
         res.setHeader('Access-Control-Allow-Origin', '*');
